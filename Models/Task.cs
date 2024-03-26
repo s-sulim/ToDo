a5c825f747e5242889ef789fb967101b3e1cd384
Svetozar Sulim <svetozarniks@gmail.com>
2024-03-26 22:32:16 +0100

    Add project files.

---
+++ Models/Task.cs
@@ -0,0 +1,37 @@
+﻿namespace ToDo.Models
+{
+    public class Task
+    {
+        public enum TaskPriority
+        {
+            High = 0,
+            Medium = 1,
+            Low = 2
+        }
+        public enum TaskState
+        {
+            ReadyToStart  = 0,
+            Doing = 1,
+            Done = 2,
+            Backlog = 3
+        }
+        public class Substep
+        {
+            public int Id { get; set; } 
+            public bool IsDone { get; set; }
+            public string? Text { get; set; }
+            public int TaskId { get; set; } 
+        }
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Text { get; set; }
+        public List<Substep>? Substeps { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public TaskPriority Priority { get; set; }
+        public TaskState State { get; set; }
+        public string? CreatedUserName { get; set; }
+        public string? AssignedUserName { get; set; }
+        public string? FinishedUserName { get; set; }
+    }
+}
